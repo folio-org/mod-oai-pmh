ff46c85d33565e5476b698027068a432b61f2b2f
Illia Daliek <illia_daliek@epam.com>
2020-04-07 20:41:58 +0300

    main code part done. TO DO: javafx.Pair -> Map.Entry

---
+++ src/main/java/org/folio/oaipmh/mappers/PropertyNameMapper.java
@@ -0,0 +1,30 @@
+package org.folio.oaipmh.mappers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Mapper is used for mapping names of frontend property names
+ * to server names.
+ */
+public class PropertyNameMapper {
+  private static Map<String,String> mapper = new HashMap<>();
+
+  static {
+    mapper.put("deletedRecordsSupport","repository.deletedRecords");
+    mapper.put("suppressedRecordsProcessing","repository.suppressedRecordsProcessing");
+    mapper.put("errorsProcessing","repository.errorsProcessing");
+    mapper.put("enableOaiService","repository.enableOaiService");
+    mapper.put("repositoryName","repository.name");
+    mapper.put("baseUrl","repository.baseURL");
+    mapper.put("administratorEmail","repository.adminEmails");
+    mapper.put("timeGranularity","repository.timeGranularity");
+    mapper.put("maxRecordsPerResponse","repository.maxRecordsPerResponse");
+    mapper.put("enableValidation","jaxb.marshaller.enableValidation");
+    mapper.put("formattedOutput","jaxb.marshaller.formattedOutput");
+  }
+
+  public static String mapFrontendKeyToServerKeyName(String keyName){
+    return mapper.get(keyName);
+  }
+}
--- src/main/java/org/folio/rest/impl/TenantAPIs.java
+++ src/main/java/org/folio/rest/impl/TenantAPIs.java
@@ -13,68 +13,85 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 import javax.ws.rs.core.Response;
 
+import org.apache.http.HttpStatus;
 import org.folio.oaipmh.helpers.storage.CQLQueryBuilder;
+import org.folio.oaipmh.mappers.PropertyNameMapper;
 import org.folio.rest.jaxrs.model.TenantAttributes;
 import org.folio.rest.tools.client.HttpClientFactory;
 import org.folio.rest.tools.client.interfaces.HttpClientInterface;
 
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import javafx.util.Pair;
-import me.escoffier.vertx.completablefuture.VertxCompletableFuture;
+
+import static java.lang.String.format;
 
 public class TenantAPIs extends TenantAPI {
   private final Logger logger = LoggerFactory.getLogger(TenantAPIs.class);
 
   private static final String X_OKAPI_URL = "x-okapi-url";
   private static final String X_OKAPI_TENANT = "x-okapi-tenant";
-  private static final String JSON_BEHAVIOUR_CONFIG_PATH = "config" + File.separator + "behaviorConfigs.json";
-  private static final String JSON_GENERAL_CONFIG_PATH = "config" + File.separator + "generalConfigs.json";
-  private static final String JSON_TECHNICAL_CONFIG_PATH = "config" + File.separator + "technicalConfigs.json";
-  private static final String MOD_CONFIGURATION_ENTRIES_PATH = "/configurations/entries/";
+  private static final String MOD_CONFIGURATION_ENTRIES_PATH = "/configurations/entries";
   private static final String BEHAVIOUR = "behaviour";
   private static final String GENERAL = "general";
   private static final String TECHNICAL = "technical";
   private static final String CONFIG_NAME = "configName";
   private static final String ACTIVE = "active";
+  private static final String CONFIGS = "configs";
+  private static final String CONFIG_DIR_NAME = "config";
+  private static final String VALUE = "value";
+  private static final int KEY_POSITION = 0;
+  private static final int VALUE_POSITION = 1;
+  private static final int CONFIG_JSON_BODY = 0;
 
   @Override
   public void postTenant(final TenantAttributes entity, final Map<String, String> headers,
       final Handler<AsyncResult<Response>> handlers, final Context context) {
-    Set<String> configSet = new HashSet<>(Arrays.asList(BEHAVIOUR, GENERAL, TECHNICAL));
-    loadConfigData();
-    super.postTenant(entity, headers, handlers, context);
+    Set<String> configNames = new HashSet<>(Arrays.asList(BEHAVIOUR, GENERAL, TECHNICAL));
+    loadConfigData(headers, configNames)
+      .thenAccept(v -> handlers.handle(Future.succeededFuture(buildResponse(HttpStatus.SC_OK))))
+      .exceptionally(throwable -> {
+        handlers.handle(Future.succeededFuture(buildResponse(HttpStatus.SC_INTERNAL_SERVER_ERROR)));
+        return null;
+      });
   }
 
-  private CompletableFuture<Response> loadConfigData(Map<String,String> headers, Set<String> configs, Context context) {
-    CompletableFuture<Response> future = new VertxCompletableFuture<>(context);
+  private CompletableFuture<Void> loadConfigData(Map<String, String> headers, Set<String> configs) {
     String okapiUrl = headers.get(X_OKAPI_URL);
     String tenant = headers.get(X_OKAPI_TENANT);
 
     HttpClientInterface httpClient = HttpClientFactory.getHttpClient(okapiUrl, tenant, true);
     List<CompletableFuture<Void>> completableFutures = new ArrayList<>();
 
-    configs.forEach(config -> {
-      completableFutures.add(requestConfig(httpClient, config, context).thenCompose(loadData))
-    });
-    }
+    configs.forEach(config -> completableFutures
+      .add(requestConfig(httpClient, headers, config).thenCompose(configPair -> postConfigIfAbsent(httpClient, headers, configPair))
+        .thenAcceptAsync(this::populateSystemProperties)));
+    return CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0]));
+  }
 
-  private CompletableFuture<Pair<String, JsonObject>> requestConfig(HttpClientInterface httpClient, String configName, Context context) {
+  private CompletableFuture<Pair<String, JsonObject>> requestConfig(HttpClientInterface httpClient, Map<String, String> headers,
+      String configName) {
     try {
-      return httpClient.request(getConfigUrl(configName))
+      return httpClient.request(getConfigUrl(configName), headers)
         .thenApply(response -> new Pair<>(configName, response.getBody()));
     } catch (Exception ex) {
+      logger.error(format("Cannot get config with configName - %s. %s", configName, ex.getMessage()));
       throw new IllegalStateException(ex);
     }
   }
@@ -87,27 +104,72 @@ public class TenantAPIs extends TenantAPI {
     return MOD_CONFIGURATION_ENTRIES_PATH.concat(queryBuilder.build());
   }
 
-  private CompletableFuture<Boolean> populateSystemPropertiesIfConfigAlreadyExists(org.folio.rest.tools.client.Response response) {
-
+  private CompletableFuture<Pair<String, JsonObject>> postConfigIfAbsent(HttpClientInterface httpClient,
+      Map<String, String> headers, Pair<String, JsonObject> configPair) {
+    JsonObject config = configPair.getValue();
+    JsonArray configs = config.getJsonArray(CONFIGS);
+    if (configs.isEmpty()) {
+      JsonObject configToPost = getJsonConfigFromResource(configPair.getKey());
+      try {
+        headers.put(HttpHeaderNames.CONTENT_TYPE.toString(), HttpHeaderValues.APPLICATION_JSON.toString());
+        return httpClient.request(HttpMethod.POST, configToPost, MOD_CONFIGURATION_ENTRIES_PATH, headers)
+          .thenApply(response -> new Pair<>(configPair.getKey(), response.getBody()));
+      } catch (Exception ex) {
+        logger.error(format("Cannot post config. %s", ex.getMessage()));
+        throw new IllegalStateException(ex);
+      }
+    } else {
+      return CompletableFuture.completedFuture(configPair);
+    }
   }
 
-  private CompletableFuture<Boolean> postConfigIfAbsent(boolean isAbsent, HttpClientInterface httpClient) {
-
+  private CompletableFuture<Pair<String, JsonObject>> populateSystemProperties(Pair<String, JsonObject> configPair) {
+    JsonObject config = configPair.getValue()
+      .getJsonArray(CONFIGS)
+      .getJsonObject(CONFIG_JSON_BODY);
+    String configValue = config.getString(VALUE);
+    Map<String, String> configKeyValueMap = getConfigKeyValueMap(configValue);
+    Properties sysProps = System.getProperties();
+    sysProps.putAll(configKeyValueMap);
+    return CompletableFuture.completedFuture(configPair);
   }
 
-  private String getResourceAsString(String resourcePath) {
-    try (InputStream is = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
-      if (is == null)
-        throw new IllegalArgumentException(String.format("Unable open the resource file %s", resourcePath));
+  private Map<String, String> getConfigKeyValueMap(final String configValue) {
+    Map<String, String> configKeyValueMap = new HashMap<>();
+    List<String> keyValuePairs = Arrays.asList(configValue.split(","));
+    keyValuePairs.forEach(keyValueString -> {
+      String[] keyValue = keyValueString.split(":");
+      configKeyValueMap.put(PropertyNameMapper.mapFrontendKeyToServerKeyName(keyValue[KEY_POSITION]), keyValue[VALUE_POSITION]);
+    });
+    return configKeyValueMap;
+  }
+
+  private JsonObject getJsonConfigFromResource(String configJsonName) {
+    String configJsonPath = CONFIG_DIR_NAME.concat(File.separator)
+      .concat(configJsonName);
+    try (InputStream is = getClass().getClassLoader()
+      .getResourceAsStream(configJsonPath)) {
+      if (is == null) {
+        String message = format("Unable open the resource file %s", configJsonPath);
+        logger.error(message);
+        throw new IllegalArgumentException(message);
+      }
       try (InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
           BufferedReader reader = new BufferedReader(isr)) {
-        return reader.lines()
+        String config = reader.lines()
           .collect(Collectors.joining(System.lineSeparator()));
+        return new JsonObject(config);
       }
     } catch (IOException ex) {
-      ex.printStackTrace();
-      return "error";
+      logger.error(ex.getMessage(), ex);
+      throw new IllegalStateException(ex);
     }
+  }
+
+  private Response buildResponse(int status) {
+    Response.ResponseBuilder responseBuilder = Response.noContent()
+      .status(status);
+    return responseBuilder.build();
   }
 
 }
