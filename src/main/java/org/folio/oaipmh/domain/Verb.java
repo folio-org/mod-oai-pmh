683698277cfbd17d1f5b3eb7c3614307ed8225b3
Illia Daliek <illia_daliek@epam.com>
2020-05-25 13:58:34 +0300

    remove validation logic from verb.java

--- src/main/java/org/folio/oaipmh/domain/Verb.java
+++ src/main/java/org/folio/oaipmh/domain/Verb.java
@@ -10,21 +10,10 @@ import static org.folio.oaipmh.Constants.METADATA_PREFIX_PARAM;
 import static org.folio.oaipmh.Constants.RESUMPTION_TOKEN_PARAM;
 import static org.folio.oaipmh.Constants.SET_PARAM;
 import static org.folio.oaipmh.Constants.UNTIL_PARAM;
-import static org.openarchives.oai._2.OAIPMHerrorcodeType.BAD_ARGUMENT;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import org.openarchives.oai._2.OAIPMHerrorType;
-
-import io.vertx.ext.web.RoutingContext;
 
 /**
  * Enum that represents OAI-PMH verbs with associated http parameters and validation logic.
@@ -52,8 +41,6 @@ public enum Verb {
   /** All possible parameters associated with the verb. */
   private Set<String> allParams;
 
-  /** ISO Date and Time with UTC offset. */
-  private static final DateTimeFormatter ISO_UTC_DATE_TIME = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
   /** Parameters which must be excluded from validation if present in http request. */
   private final Set<String> excludeParams = of(VERB_PARAM, PARAM_API_KEY, PATH_API_KEY);
 
@@ -73,28 +60,6 @@ public enum Verb {
     allParams = concat(concat(requiredParams.stream(), optionalParams.stream()), excludeParams.stream())
       .collect(toSet());
     allParams.add(exclusiveParam);
-  }
-
-  /**
-   * Checks if given {@link RoutingContext}'s http parameters are valid for current verb.
-   *
-   * @param ctx {@link RoutingContext} to validate
-   * @return list of {@link OAIPMHerrorType} errors if any or empty list otherwise
-   */
-  public List<OAIPMHerrorType> validate(RoutingContext ctx) {
-    List<OAIPMHerrorType> errors = new ArrayList<>();
-
-    validateIllegalParams(ctx, errors);
-    validateExclusiveParam(ctx, errors);
-
-    if (ctx.request().getParam(FROM_PARAM) != null) {
-      validateDatestamp(FROM_PARAM, ctx.request().getParam(FROM_PARAM), errors);
-    }
-    if (ctx.request().getParam(UNTIL_PARAM) != null) {
-      validateDatestamp(UNTIL_PARAM, ctx.request().getParam(UNTIL_PARAM), errors);
-    }
-
-    return errors;
   }
 
   public static Verb fromName(String name) {
@@ -124,45 +89,6 @@ public enum Verb {
   @Override
   public String toString() {
     return this.name;
-  }
-
-
-  private void validateIllegalParams(RoutingContext ctx, List<OAIPMHerrorType> errors) {
-    ctx.request().params().entries().stream()
-      .map(Map.Entry::getKey)
-      .filter(param -> !allParams.contains(param))
-      .forEach(param -> errors.add(new OAIPMHerrorType()
-        .withCode(BAD_ARGUMENT)
-        .withValue("Verb '" + name + "', illegal argument: " + param)));
-  }
-
-  private void validateExclusiveParam(RoutingContext ctx, List<OAIPMHerrorType> errors) {
-    if (exclusiveParam != null && ctx.request().getParam(exclusiveParam) != null) {
-      ctx.request().params().entries().stream()
-        .map(Map.Entry::getKey)
-        .filter(p -> !excludeParams.contains(p))
-        .filter(p -> !exclusiveParam.equals(p))
-        .findAny()
-        .ifPresent(param -> errors.add(new OAIPMHerrorType()
-          .withCode(BAD_ARGUMENT)
-          .withValue("Verb '" + name + "', argument '" + exclusiveParam +
-            "' is exclusive, no others maybe specified with it.")));
-    }
-  }
-
-  private void validateDatestamp(String paramName, String paramValue, List<OAIPMHerrorType> errors) {
-    try {
-      LocalDateTime.parse(paramValue, ISO_UTC_DATE_TIME);
-    } catch (DateTimeParseException e) {
-      // The repository must support YYYY-MM-DD granularity so try to parse date only
-      try {
-        LocalDate.parse(paramValue);
-      } catch (DateTimeParseException ex) {
-        errors.add(new OAIPMHerrorType()
-          .withCode(BAD_ARGUMENT)
-          .withValue("Bad datestamp format for '" + paramName + "' argument."));
-      }
-    }
   }
 }
 
