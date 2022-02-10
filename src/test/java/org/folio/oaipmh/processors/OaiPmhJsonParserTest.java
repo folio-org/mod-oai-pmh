52e89ccf937da23d6c4820bc29a1fb7d287a9a79
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2022-02-10 09:55:48 +0300

    MODOAIPMH-391 - fix sonar issues

--- src/test/java/org/folio/oaipmh/processors/OaiPmhJsonParserTest.java
+++ src/test/java/org/folio/oaipmh/processors/OaiPmhJsonParserTest.java
@@ -11,8 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class OaiPmhJsonParserTest {
 
   @Test
-  public void testParserHandleValidData() {
-
+ void testParserHandleValidData() {
     var instances = "{  \"instanceId\": \"e6bc03c6-c137-4221-b679-a7c5c31f986c\" , \"source\": \"FOLIO\", " +
       "\"updatedDate\": \"2020-06-15T11:07:48.563Z\",  \"deleted\": \"false\",  \"suppressFromDiscovery\": \"false\"}" +
       "\n\r { \"instanceId\" : \"a7bc91c6-c137-4221-b679-a7c5c31f986c\", \"source\": \"FOLIO\"," +
@@ -20,30 +19,29 @@ public class OaiPmhJsonParserTest {
     var errors = new ArrayList<>();
     var events = new ArrayList<JsonEvent>();
 
-    var oaiParser = new OaiPmhJsonParser()
+    var jsonParser = new OaiPmhJsonParser()
       .objectValueMode()
       .handler(events::add)
       .exceptionHandler(errors::add);
-    oaiParser.write(Buffer.buffer(instances)).end();
+    jsonParser.write(Buffer.buffer(instances)).end();
     assertEquals(0, errors.size());
     assertEquals(2, events.size());
   }
 
   @Test
-  public void testParserHandleNotValidData() {
+  void testParserHandleNotValidData() {
     var instances = "{  \"instanceId\": \"e6bc03c6-c137-4221-b679-a7c5c31f986c\" , \"source\": \"FOLIO\", " +
       "\"updatedDate\": \"2020-06-15T11:07:48.563Z\",  \"deleted\": \"false\",  \"suppressFromDiscovery\": \"false\"}" +
       "\n\r { \"instanceId\"  \"a7bc91c6-c137-4221-b679-a7c5c31f986c\", \"source\": \"FOLIO\"," +
       " \"updatedDate\": \"2020-06-15T11:07:48.732Z\",  \"deleted\": \"false\" : \"suppressFromDiscovery\": \"false\"}";
-
     var errors = new ArrayList<>();
     var events = new ArrayList<JsonEvent>();
 
-    var oaiParser = new OaiPmhJsonParser()
+    var jsonParser = new OaiPmhJsonParser()
       .objectValueMode()
       .handler(events::add)
       .exceptionHandler(errors::add);
-    oaiParser.write(Buffer.buffer(instances)).end();
+    jsonParser.write(Buffer.buffer(instances)).end();
     assertEquals(2, errors.size());
     assertEquals(0, events.size());
   }
