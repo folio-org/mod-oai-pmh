7efed9565507ce0ce6a8a5c24c35c671f6416b44
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2023-07-05 13:14:31 +0300

    MODOAIPMH-524 Fixed error

--- src/main/java/org/folio/oaipmh/helpers/client/ConsortiaClient.java
+++ src/main/java/org/folio/oaipmh/helpers/client/ConsortiaClient.java
@@ -7,10 +7,12 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.oaipmh.Request;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class ConsortiaClient extends InventoryClient {
 
   private static final Logger logger = LogManager.getLogger(ConsortiaClient.class);
