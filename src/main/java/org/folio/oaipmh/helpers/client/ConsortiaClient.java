7ad6ee5b75e2a63fba4d8ffae62095c2a9e398db
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2023-07-06 10:12:00 +0300

    MODOAIPMH-524 Removed redundant

--- src/main/java/org/folio/oaipmh/helpers/client/ConsortiaClient.java
+++ src/main/java/org/folio/oaipmh/helpers/client/ConsortiaClient.java
@@ -9,16 +9,12 @@ import org.apache.logging.log4j.Logger;
 import org.folio.oaipmh.Request;
 import org.springframework.stereotype.Component;
 
-import static java.lang.String.format;
-
 @Component
 public class ConsortiaClient extends InventoryClient {
 
   private static final Logger logger = LogManager.getLogger(ConsortiaClient.class);
 
   private static final String USER_TENANTS_ENDPOINT = "/user-tenants?limit=1";
-  private static final String CONSORTIUM_ENDPOINT = "/consortia";
-  private static final String TENANTS_ENDPOINT = "/consortia/%s/tenants";
 
   public JsonArray getUserTenants(Request request) {
     var endpoint = request.getOkapiUrl() + USER_TENANTS_ENDPOINT;
@@ -27,32 +23,6 @@ public class ConsortiaClient extends InventoryClient {
     try (CloseableHttpResponse response = HttpClients.createDefault().execute(httpGet)) {
       var jsonObject = getResponseEntity(response);
       return jsonObject.getJsonArray("userTenants");
-    } catch (Exception exception) {
-      logger.error("Exception while calling {}", httpGet.getURI(), exception);
-    }
-    return new JsonArray();
-  }
-
-  public JsonArray getConsortiums(Request request) {
-    var endpoint = request.getOkapiUrl() + CONSORTIUM_ENDPOINT;
-    HttpGet httpGet = httpGet(request, endpoint);
-    logger.info("Calling GET {}", endpoint);
-    try (CloseableHttpResponse response = HttpClients.createDefault().execute(httpGet)) {
-      var jsonObject = getResponseEntity(response);
-      return jsonObject.getJsonArray("consortia");
-    } catch (Exception exception) {
-      logger.error("Exception while calling {}", httpGet.getURI(), exception);
-    }
-    return new JsonArray();
-  }
-
-  public JsonArray getTenants(Request request, String consortiumId) {
-    var endpoint = request.getOkapiUrl() + format(TENANTS_ENDPOINT, consortiumId);
-    HttpGet httpGet = httpGet(request, endpoint);
-    logger.info("Calling GET {}", endpoint);
-    try (CloseableHttpResponse response = HttpClients.createDefault().execute(httpGet)) {
-      var jsonObject = getResponseEntity(response);
-      return jsonObject.getJsonArray("tenants");
     } catch (Exception exception) {
       logger.error("Exception while calling {}", httpGet.getURI(), exception);
     }
