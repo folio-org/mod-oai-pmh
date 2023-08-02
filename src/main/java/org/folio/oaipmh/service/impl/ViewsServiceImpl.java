627ea3f94a3dacf137dbcbba177a70a91ac9cb8c
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2023-08-02 19:42:23 +0300

    MODOAIPMH-492 Added logging query time

--- src/main/java/org/folio/oaipmh/service/impl/ViewsServiceImpl.java
+++ src/main/java/org/folio/oaipmh/service/impl/ViewsServiceImpl.java
@@ -2,6 +2,8 @@ package org.folio.oaipmh.service.impl;
 
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.oaipmh.dao.ViewsDao;
 import org.folio.oaipmh.service.ViewsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ViewsServiceImpl implements ViewsService {
 
+  protected final Logger logger = LogManager.getLogger(getClass());
+
   @Autowired
   private ViewsDao viewsDao;
 
   @Override
   public Future<JsonArray> query(String query, String tenantId) {
-    return viewsDao.query(query, tenantId);
+    long t1 = System.nanoTime();
+    return viewsDao.query(query, tenantId).onComplete(hand -> {
+      logger.info("Total time for query: {} sec", (System.nanoTime() - t1) / 1_000_000_000);
+    });
   }
 }
