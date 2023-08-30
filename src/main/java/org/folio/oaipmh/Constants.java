3fdc5ca93e974fb833770fd979bec93771eb3332
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2023-08-30 17:36:40 +0300

    MODOAIPMH-492 - fixed mergeIn

--- src/main/java/org/folio/oaipmh/Constants.java
+++ src/main/java/org/folio/oaipmh/Constants.java
@@ -157,4 +157,6 @@ public final class Constants {
   public static final String HTTPS = "https:";
   public static final String INSTANCE_ID_FROM_VIEW_RESPONSE = "instance_id";
   public static final String MARC_RECORD_FROM_VIEW_RESPONSE = "marc_record";
+  public static final String SOURCE_RECORDS = "sourceRecords";
+  public static final String TOTAL_RECORDS = "totalRecords";
 }
