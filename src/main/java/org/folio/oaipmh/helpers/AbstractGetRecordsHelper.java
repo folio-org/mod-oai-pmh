d360fad5cbd220082f26670521b7876cd9d0eb4d
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2023-11-28 13:41:39 +0200

    Revert "MODOAIPMH-549-TEST"
    This reverts commit c0430c50c6b59351f3961cdb17215e2413701290.

--- src/main/java/org/folio/oaipmh/helpers/AbstractGetRecordsHelper.java
+++ src/main/java/org/folio/oaipmh/helpers/AbstractGetRecordsHelper.java
@@ -255,7 +255,7 @@ public abstract class AbstractGetRecordsHelper extends AbstractHelper {
           logger.info("Number of SRS records from central tenant: {}", sourceRecordsCentral.size());
           logger.info("Number of SRS records from local tenant: {}", sourceRecordsLocal.size());
           if (nonNull(sourceRecordsCentral)) {
-            if (request.getVerb() == VerbType.GET_RECORD) {
+            if (!sourceRecordsCentral.isEmpty() && request.getVerb() == VerbType.GET_RECORD) {
               sourceRecordsLocal.clear();
             }
             sourceRecordsLocal.addAll(sourceRecordsCentral);
