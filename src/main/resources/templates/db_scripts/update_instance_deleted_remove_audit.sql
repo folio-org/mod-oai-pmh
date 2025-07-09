29405d3587f3eb1d4252c4feaa1fbd771351525e
obozhko-folio <83696213+obozhko-folio@users.noreply.github.com>
2025-07-09 09:36:42 +0300

    MODOAIPMH-586 - Performance issues for deletes on full harvest (#412)
    * MODOAIPMH-586 Removed audit

--- src/main/resources/templates/db_scripts/update_instance_deleted_remove_audit.sql
+++ src/main/resources/templates/db_scripts/update_instance_deleted_remove_audit.sql
@@ -1,11 +1,3 @@
-CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.get_instances_from_inventory AS
-SELECT * FROM ${myuniversity}_mod_inventory_storage.instance
-WHERE jsonb ->> 'deleted' = 'false';
-
-CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.get_instances_from_inventory_deleted AS
-SELECT * FROM ${myuniversity}_mod_inventory_storage.instance
-WHERE jsonb ->> 'deleted' = 'true';
-
 CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.get_instances_with_marc_records_deleted AS
 SELECT (jsonb ->> 'id')::uuid                                                                                              instance_id,
       marc_record.content                                                                                                     marc_record,
@@ -20,17 +12,6 @@ SELECT (jsonb ->> 'id')::uuid                                                   
       INNER JOIN ${myuniversity}_${mymodule}.get_instances_from_inventory instance_record
       ON instance_record.id = record_lb.external_id
       INNER JOIN ${myuniversity}_${mymodule}.get_marc_records marc_record  ON marc_record.id = record_lb.id
-UNION ALL
-SELECT (jsonb -> 'record' ->> 'id')::uuid                                                                                     instance_id,
-      null                                                                                                                    marc_record,
-      instance_record.jsonb                                                                                                   instance_record,
-      instance_record.jsonb -> 'record' ->> 'source'                                                                          source,
-      ${myuniversity}_mod_inventory_storage.strToTimestamp(instance_record.jsonb ->> 'createdDate') instance_updated_date,
-      ${myuniversity}_mod_inventory_storage.strToTimestamp(instance_record.jsonb ->> 'createdDate') instance_created_date,
-      false                                                                                                                   suppress_from_discovery_srs,
-      COALESCE((instance_record.jsonb ->> 'discoverySuppress')::bool, false)                                                  suppress_from_discovery_inventory,
-      true                                                                                                                    deleted
-      FROM ${myuniversity}_${mymodule}.get_deleted_instances instance_record
 UNION ALL
 SELECT instance_record.id                                                                                                          instance_id,
       null                                                                                                                         marc_record,
