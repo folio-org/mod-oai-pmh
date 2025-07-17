2f6e91413ba7f11735927f1122df10c696b3b645
obozhko-folio <Oleksandr_Bozhko@epam.com>
2025-07-17 13:50:57 +0300

    MODOAIPMH-624 Added check for null

--- src/main/resources/templates/db_scripts/update_instance_deleted.sql
+++ src/main/resources/templates/db_scripts/update_instance_deleted.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.get_instances_from_inventory AS
 SELECT * FROM ${myuniversity}_mod_inventory_storage.instance
-WHERE jsonb ->> 'deleted' IS NULL OR jsonb ->> 'deleted' = 'false';
+WHERE jsonb ->> 'deleted' = 'false';
 
 CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.get_instances_from_inventory_deleted AS
 SELECT * FROM ${myuniversity}_mod_inventory_storage.instance
