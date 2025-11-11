51bfc3674fa85c51339317cf52193f999d8bb765
kjain110 <114437186+kjain110@users.noreply.github.com>
2025-11-11 09:00:35 -0600

    Revert "Revert "Revert "Revert "MODOAIPMH-621: Migrate settings from mod-conf…"
    This reverts commit ea2d190bd3207a01d6714d36912a72d69dc5c0fd.

---
+++ src/main/java/org/folio/oaipmh/service/ConfigurationSettingsService.java
@@ -0,0 +1,21 @@
+package org.folio.oaipmh.service;
+
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+
+public interface ConfigurationSettingsService {
+
+  Future<JsonObject> getConfigurationSettingsById(String id, String tenantId);
+
+  Future<JsonObject> getConfigurationSettingsByName(String configName, String tenantId);
+
+  Future<JsonObject> updateConfigurationSettingsById(String id, JsonObject entry,
+                                                     String tenantId, String userId);
+
+  Future<JsonObject> saveConfigurationSettings(JsonObject entry, String tenantId, String userId);
+
+  Future<Boolean> deleteConfigurationSettingsById(String id, String tenantId);
+
+  Future<JsonObject> getConfigurationSettingsList(int offset, int limit, String tenantId);
+
+}
