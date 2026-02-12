a24957f8651fda5144483a1483c46a7dee89d84d
Siarhei Charniak <siarhei_charniak@epam.com>
2026-02-12 13:49:41 +0300

    MODOAIPMH-655

--- src/main/java/org/folio/oaipmh/helpers/RepositoryConfigurationUtil.java
+++ src/main/java/org/folio/oaipmh/helpers/RepositoryConfigurationUtil.java
@@ -62,9 +62,6 @@ public class RepositoryConfigurationUtil {
             String configKey = response != null && response.containsKey("configurationSettings")
                 ? "configurationSettings" : CONFIGS;
 
-            logger.info("Using config key: {}, response contains key: {}",
-                configKey, response != null && response.containsKey(configKey));
-
             if (response != null && response.containsKey(configKey)) {
               response.getJsonArray(configKey)
                   .stream()
@@ -74,18 +71,12 @@ public class RepositoryConfigurationUtil {
                   .forEach(entry -> {
                     JsonObject configValue = entry.getJsonObject("configValue");
                     if (configValue != null) {
-                      logger.info("Found configValue object: {}", configValue.encodePrettily());
-                      logger.info("configValue map size: {}", configValue.getMap().size());
                       configValue.fieldNames().forEach(key -> {
                         Object value = configValue.getValue(key);
                         String mappedKey = org.folio.oaipmh.mappers.PropertyNameMapper
                             .mapFrontendKeyToServerKey(key);
                         String stringValue = value != null ? value.toString() : null;
-                        logger.info("Mapping: {} -> {} = {} (type: {})",
-                            key, mappedKey, stringValue,
-                            value != null ? value.getClass().getSimpleName() : "null");
                         config.put(mappedKey, stringValue);
-                        logger.info("Config now has {} keys", config.size());
                       });
                     } else {
                       logger.warn("configValue is null for entry: {}", entry.encodePrettily());
