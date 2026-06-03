e88b8545ee582f0c499e80e1d8a1af175c6f4c39
obozhko-folio <Oleksandr_Bozhko@epam.com>
2026-06-03 13:34:15 +0300

    MODOAIPMH-667 Added srs discovery suppr as inventory

--- src/main/resources/templates/db_scripts/update_deleted_remove_srs.sql
+++ src/main/resources/templates/db_scripts/update_deleted_remove_srs.sql
@@ -5,6 +5,7 @@ SELECT instance_record.id                                                       
       instance_record.jsonb ->> 'source'                                                                                           source,
       instance_record.complete_updated_date AS                                                                                     instance_updated_date,
       ${myuniversity}_mod_inventory_storage.strtotimestamp((instance_record.jsonb -> 'metadata'::text) ->> 'createdDate'::text) AS instance_created_date,
+      COALESCE((instance_record.jsonb ->> 'discoverySuppress')::bool, false)                                                       suppress_from_discovery_srs,
       COALESCE((instance_record.jsonb ->> 'discoverySuppress')::bool, false)                                                       suppress_from_discovery_inventory,
       true                                                                                                                         deleted
       FROM ${myuniversity}_${mymodule}.get_instances_from_inventory_deleted instance_record;
