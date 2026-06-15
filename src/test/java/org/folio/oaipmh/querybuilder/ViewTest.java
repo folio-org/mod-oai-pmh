0f9fb21b906a6446f9fa259405f94df54dcf67dd
Mikita Siadykh <mikita_siadykh@epam.com>
2026-06-15 14:00:03 +0300

    backport workflow

--- src/test/java/org/folio/oaipmh/querybuilder/ViewTest.java
+++ src/test/java/org/folio/oaipmh/querybuilder/ViewTest.java
@@ -44,7 +44,7 @@ class ViewTest {
   private static final Network network = Network.newNetwork();
 
   @Container
-  private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+  private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14-alpine")
     .withNetwork(network)
     .withNetworkAliases("postgres")
     .withExposedPorts(5432)
