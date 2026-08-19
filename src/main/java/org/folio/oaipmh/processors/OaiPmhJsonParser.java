10f4b67723a15ed7e71e6e223b9364e8f50edf40
pawelmol <pawel@mol.com.pl>
2026-08-19 13:33:50 +0200

    Update OaiPmhJsonParser.java

--- src/main/java/org/folio/oaipmh/processors/OaiPmhJsonParser.java
+++ src/main/java/org/folio/oaipmh/processors/OaiPmhJsonParser.java
@@ -36,7 +36,8 @@ public class OaiPmhJsonParser extends JsonParserImpl {
     } catch (DecodeException e) {
       var errorResolver = new JsonParserErrorResolver(data.toString(), e.getLocalizedMessage());
       logger.error(e.getLocalizedMessage());
-      logger.error("Decode parser exception: Error position at error part of json is {}", errorResolver.getErrorPosition());
+      logger.error("Decode parser exception: Error position at error part of json is {}",
+          errorResolver.getErrorPosition());
       logger.error(errorResolver.getErrorPart());
       errors.add(errorResolver.getErrorPart());
       if (handle(e)) {
