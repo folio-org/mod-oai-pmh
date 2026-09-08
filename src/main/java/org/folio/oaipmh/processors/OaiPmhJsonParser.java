78e508c67a898fd8f604fc7cc58524390507ab03
Paweł Habas <pawel@mol.com.pl>
2026-09-08 14:23:26 +0200

    MODOAIPMH-672 Invalid special characters encoding (#482)
    Fix: feed raw Buffer to super.handle(data)

--- src/main/java/org/folio/oaipmh/processors/OaiPmhJsonParser.java
+++ src/main/java/org/folio/oaipmh/processors/OaiPmhJsonParser.java
@@ -31,11 +31,10 @@ public class OaiPmhJsonParser extends JsonParserImpl {
 
   @Override
   public void handle(Buffer data) {
-    var normalized =  data.toString().replaceAll("([\\r\\n])", "");
     try {
-      super.handle(Buffer.buffer(normalized));
+      super.handle(data);
     } catch (DecodeException e) {
-      var errorResolver = new JsonParserErrorResolver(normalized, e.getLocalizedMessage());
+      var errorResolver = new JsonParserErrorResolver(data.toString(), e.getLocalizedMessage());
       logger.error(e.getLocalizedMessage());
       logger.error("Decode parser exception: Error position at error part of json is {}",
           errorResolver.getErrorPosition());
